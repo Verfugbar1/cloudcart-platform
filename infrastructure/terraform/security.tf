35948c4f9aecfe93cd34831b2d60f6c8179a5c45
osama <tusama716@gmail.com>
2026-08-29 18:23:46 +0200

    fix: remove duplicate terraform variable

--- infrastructure/terraform/security.tf
+++ infrastructure/terraform/security.tf
@@ -1,51 +1,52 @@
 resource "azurerm_network_security_group" "app" {
-  name                = "${var.project_name}-app-nsg"
+  name                = "${local.name_prefix}-app-nsg"
   location            = azurerm_resource_group.cloudcart.location
   resource_group_name = azurerm_resource_group.cloudcart.name
+
+  tags = local.common_tags
 }
+
 resource "azurerm_network_security_rule" "http" {
-  name                        = "AllowHTTP"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  name                       = "AllowHTTP"
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
   resource_group_name         = azurerm_resource_group.cloudcart.name
   network_security_group_name = azurerm_network_security_group.app.name
 }
 
 resource "azurerm_network_security_rule" "https" {
-  name                        = "AllowHTTPS"
-  priority                    = 110
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  name                       = "AllowHTTPS"
+  priority                   = 110
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "443"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
   resource_group_name         = azurerm_resource_group.cloudcart.name
   network_security_group_name = azurerm_network_security_group.app.name
 }
 
-variable "admin_source_ip" {
-  description = "Public IPv4 address allowed to SSH to the CloudCart VM."
-  type        = string
-}
-
 resource "azurerm_network_security_rule" "ssh" {
-  name                        = "AllowSSH"
-  priority                    = 120
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "${var.admin_source_ip}/32"
-  destination_address_prefix  = "*"
+  name                       = "AllowSSH"
+  priority                   = 120
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "${var.admin_source_ip}/32"
+  destination_address_prefix = "*"
+
   resource_group_name         = azurerm_resource_group.cloudcart.name
   network_security_group_name = azurerm_network_security_group.app.name
 }
@@ -53,4 +54,30 @@ resource "azurerm_network_security_rule" "ssh" {
 resource "azurerm_subnet_network_security_group_association" "app" {
   subnet_id                 = azurerm_subnet.app.id
   network_security_group_id = azurerm_network_security_group.app.id
+}
+
+resource "azurerm_public_ip" "vm" {
+  name                = "${local.name_prefix}-vm-public-ip"
+  location            = azurerm_resource_group.cloudcart.location
+  resource_group_name = azurerm_resource_group.cloudcart.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_network_interface" "vm" {
+  name                = "${local.name_prefix}-vm-nic"
+  location            = azurerm_resource_group.cloudcart.location
+  resource_group_name = azurerm_resource_group.cloudcart.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.app.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm.id
+  }
+
+  tags = local.common_tags
 }
