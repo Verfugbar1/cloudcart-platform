7a46e30d3a7af51093be4defe2ca5a13c5e46ce8
osama <tusama716@gmail.com>
2026-08-29 18:03:38 +0200

    ci: validate terraform

--- infrastructure/terraform/compute.tf
+++ infrastructure/terraform/compute.tf
@@ -1,9 +1,10 @@
 resource "azurerm_linux_virtual_machine" "cloudcart" {
-  name                = "${var.project_name}-vm"
+  name                = "${local.name_prefix}-vm"
   resource_group_name = azurerm_resource_group.cloudcart.name
   location            = azurerm_resource_group.cloudcart.location
-  size                = "Standard_B2s_v2"
-  admin_username      = "azureuser"
+
+  size           = var.vm_size
+  admin_username = var.admin_username
 
   network_interface_ids = [
     azurerm_network_interface.vm.id
@@ -13,7 +14,7 @@ resource "azurerm_linux_virtual_machine" "cloudcart" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file(pathexpand("~/.ssh/cloudcart_vm.pub"))
+    public_key = var.ssh_public_key
   }
 
   os_disk {
@@ -27,4 +28,16 @@ resource "azurerm_linux_virtual_machine" "cloudcart" {
     sku       = "22_04-lts-gen2"
     version   = "latest"
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.common_tags
+}
+
+resource "azurerm_role_assignment" "vm_acr_pull" {
+  scope                = azurerm_container_registry.cloudcart.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_virtual_machine.cloudcart.identity[0].principal_id
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,11 +1,39 @@
+variable "project_name" {
+  description = "Name of the project."
+  type        = string
+  default     = "cloudcart"
+}
+
+variable "environment" {
+  description = "Deployment environment."
+  type        = string
+  default     = "dev"
+}
+
 variable "location" {
-  description = "Azure region for CloudCart infrastructure."
+  description = "Azure region."
   type        = string
   default     = "West Europe"
 }
 
-variable "project_name" {
-  description = "Project name used in Azure resource naming."
+variable "admin_username" {
+  description = "Linux administrator username."
   type        = string
-  default     = "cloudcart"
+  default     = "azureuser"
+}
+
+variable "admin_source_ip" {
+  description = "Public IPv4 address allowed to SSH to the VM."
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Azure VM size."
+  type        = string
+  default     = "Standard_B2s_v2"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key used to access the CloudCart VM"
+  type        = string
 }
